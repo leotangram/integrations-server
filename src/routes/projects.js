cca1277ca9c66c786ab5d194187e9d94b3379d01
Leonardo Omaña <leotangram@gmail.com>
2020-07-04 10:32:00 -0500

    Project routs

---
+++ src/routes/projects.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const projectController = require('../controllers/projectController')
+const auth = require('../middleware/auth')
+const { check } = require('express-validator')
+
+router.post(
+  '/',
+  auth,
+  [check('name', 'El nombre del proyecto es obligatorio').not().isEmpty()],
+  projectController.createProject
+)
+router.get('/', auth, projectController.getProjects)
+router.put(
+  '/:id',
+  auth,
+  [check('name', 'El nombre del proyecto es obligatorio').not().isEmpty()],
+  projectController.updateProject
+)
+router.delete('/:id', auth, projectController.deleteProject)
+
+module.exports = router
